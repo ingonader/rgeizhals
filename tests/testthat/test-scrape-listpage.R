88da601bec51663b4b953d19e4500993865cb1d0
Ingo Nader <ingo.nader@gmail.com>
2018-10-17 21:31:25 +0200

    Reorganize unit tests

--- tests/testthat/test-scrape-listpage.R
+++ tests/testthat/test-scrape-listpage.R
@@ -1,21 +1,15 @@
-context("test-scrape-listpage")
+context("scrape listpage")
 
 listpagehtml_trockner_01 <- xml2::read_html("gh-lst-trockn-01.html")
 listpagehtml_trockner_02 <- xml2::read_html("gh-lst-trockn-02.html")
 listpagehtml_nas_01 <- xml2::read_html("gh-lst-nas-01.html")
 
-test_that("next listpage is correctly identified", {
-  r <- parse_next_listpage_url(listpagehtml_trockner_01)
-  expect_equal(r, "https://geizhals.at/?cat=hwaeschtr&xf=1027_W%E4rmepumpentrockner~1296_10~1747_8~7641_40~7653_9&pg=2#productlist")
+## ========================================================================= ##
+## parse_product_names()
+## ========================================================================= ##
 
-  r <- parse_next_listpage_url(listpagehtml_trockner_02)
-  expect_equal(r, NA)
-
-  r <- parse_next_listpage_url(listpagehtml_nas_01)
-  expect_equal(r, NA)
-})
-
-test_that("product names are correctly identified", {
+test_that("parse_product_names()
+          identifies product names correctly", {
   r <- parse_product_names(listpagehtml_trockner_02)
   expect_equal(r,
                c("Miele TCF620 WP Eco Wärmepumpentrockner",
@@ -24,12 +18,32 @@ test_that("product names are correctly identified", {
                  "Miele TWF620 WP Eco Wärmepumpentrockner",
                  "Miele TMM840 WP Wärmepumpentrockner",
                  "Miele TMM843 WP SFinish&Eco Wifi Wärmepumpentrockner")
-               )
+  )
   r <- parse_product_names(listpagehtml_nas_01)
   expect_equal(r,
                c("Asustor AS6210T, 4x Gb LAN (90IX00X1-BW3S10)",
                  "QNAP Turbo Station TS-EC1080 Pro, 4x Gb LAN")
   )
 })
+
+## ========================================================================= ##
+## parse_next_listpage_url()
+## ========================================================================= ##
+
+test_that("parse_next_listpage_url()
+          identifes next listpage correctly", {
+  r <- parse_next_listpage_url(listpagehtml_trockner_01)
+  expect_equal(r, "https://geizhals.at/?cat=hwaeschtr&xf=1027_W%E4rmepumpentrockner~1296_10~1747_8~7641_40~7653_9&pg=2#productlist")
+})
+
+test_that("parse_next_listpage_url()
+          identifies non-existing next listpage correctly as NA", {
+  r <- parse_next_listpage_url(listpagehtml_trockner_02)
+  expect_equal(r, NA)
+
+  r <- parse_next_listpage_url(listpagehtml_nas_01)
+  expect_equal(r, NA)
+})
+
 
 
